06b010a18c75468776dbaf8354c9650158e68a65
kerlos <kerlosaitf16@gmail.com>
2025-02-10 23:52:09 +0200

    Add search view route to app navigation

---
+++ lib/Features/search/Presentation/views/Search_view.dart
@@ -0,0 +1,15 @@
+import 'package:bookly/Features/search/Presentation/views/widget/SearchViewBody.dart';
+import 'package:flutter/material.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SearchViewBody(),
+      ),
+    );
+  }
+}
--- lib/core/utlis/app_routrs.dart
+++ lib/core/utlis/app_routrs.dart
@@ -1,16 +1,22 @@
 import 'package:bookly/Features/home/presentation/views/Home_view.dart';
 import 'package:bookly/Features/home/presentation/views/book_deatils_view.dart';
+import 'package:bookly/Features/search/Presentation/views/Search_view.dart';
 import 'package:bookly/Features/splash/Presentation/Views/Splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRoutes {
   static const String homeView = '/homeView';
   static const String bookDetailsView = '/bookDetailsView';
+  static const String searchView = '/searchView';
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: '/',
         builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: searchView,
+        builder: (context, state) => const SearchView(),
       ),
       GoRoute(
         path: homeView,
