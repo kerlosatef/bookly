3754dca5bbd4bb53dec231ad9405a2dcc6b6792d
kerlos <kerlosaitf16@gmail.com>
2025-02-12 20:15:35 +0200

    Add cached_network_image dependency and update Customebookimage widget for improved image loading

--- lib/Features/home/presentation/views/widget/Custome_book_item.dart
+++ lib/Features/home/presentation/views/widget/Custome_book_item.dart
@@ -1,20 +1,21 @@
 import 'package:bookly/core/utlis/assets.dart';
+import 'package:bookly/core/widget/custome_loading.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Customebookimage extends StatelessWidget {
   const Customebookimage({super.key, required this.imageUrl});
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(imageUrl),
-            fit: BoxFit.cover,
-          ),
-          borderRadius: BorderRadius.circular(14),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: CachedNetworkImage(
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => Icon(Icons.error),
         ),
       ),
     );
