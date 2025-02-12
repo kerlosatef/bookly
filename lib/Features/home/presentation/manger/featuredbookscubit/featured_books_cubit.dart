92e483efedfdc525c01e76c4db2a9847f5f11fbd
kerlos <kerlosaitf16@gmail.com>
2025-02-12 17:57:46 +0200

    Implement featured books cubit with repository integration

--- lib/Features/home/presentation/manger/featuredbookscubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/featuredbookscubit/featured_books_cubit.dart
@@ -1,9 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/data/Models/book_model/book_model.dart';
+import 'package:bookly/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBookCubit extends Cubit<FeaturedBookState> {
-  FeaturedBookCubit() : super(FeaturedBookInitial());
+  FeaturedBookCubit(this.homeRepo) : super(FeaturedBookInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBookLoading());
+    var result = await homeRepo.fetchFeaturedBook();
+    result.fold(
+      (failure) => emit(FeaturedBookfailuer(failure.message)),
+      (books) => emit(FeaturedBookSuccess(books)),
+    );
+  }
 }
